ac3f820813f5481d87a54556cc7c24d150205308
abhiramm <abhiramasundram2007@gmail.com>
2015-11-01 23:21:24 +0530

    added multiple keywords

--- app/api/generator/api.rb
+++ app/api/generator/api.rb
@@ -6,14 +6,22 @@ module Generator
                 format :json
                 prefix :api
                 get :keywords do 
-                keyword = params[:keyword]  
-                uri = URI.parse("http://api.datamuse.com/words?rd=#{keyword}")
-                puts "uri #{uri.read}"
-                json_values = JSON.parse(uri.read)
-                automated_words = json_values.collect {|x| x["word"]}
-                final_output = automated_words << params[:keyword]
-                final_json = {"key_words" => final_output}
+                        keywords = {}
+                        #                keyword = params[:keyword]  
+                        i = 1
+                        params.each do |k|
+                                @word = params["key#{i}"]
+                                uri = URI.parse("http://api.datamuse.com/words?rd=#{@word}")
+                                puts "uri #{uri.read}"
+                                json_values = JSON.parse(uri.read)
+                                automated_words = json_values.collect {|x| x["word"]}
+                                final_output = automated_words << params["key#{i}"]
+                                keywords[@word] = final_output
+                                i = i+1
+                        end
+                        keywords
+                        #          final_json = {"key_words" => final_output}
 
-             end
+                end
         end
 end
